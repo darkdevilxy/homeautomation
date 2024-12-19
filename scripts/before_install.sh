ed70eb20c7c348b6e80a376367722459560e195a
bhumagainxy <bhumagainxy@gmail.com>
2024-12-19 11:52:32 +0000

    Attemped scripts fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,5 +2,5 @@
 sudo yum update -y
 sudo yum install -y nodejs npm
 
-sudo chown ec2-user:ec2-user /var/www/html
+sudo chown -R ec2-user:ec2-user /var/www/html
 sudo chmod +x /var/www/html/scripts/start_application.sh
