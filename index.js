48b097617b7e43a2d0e7b91e9f080d95efc2afc4
bhumagainxy <bhumagainxy@gmail.com>
2024-12-19 10:06:26 +0000

    Fixed directory structure. Node now exposes html file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
-  res.send('Hello from your CI/CD pipeline!');
+  res.sendFile(path.join(__dirname,'public', 'index.html'));
 });
 
 app.listen(port, () => {
