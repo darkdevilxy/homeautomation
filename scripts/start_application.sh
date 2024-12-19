e465e9ee724b3af5399662fa14c710318ad9bcb8
bhumagainxy <bhumagainxy@gmail.com>
2024-12-19 11:35:49 +0000

    script fix

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
+
 cd /var/www/html
-npm install
+
+# Set verbose output for npm install
+npm install --verbose
+
+if [ $? -ne 0 ]; then
+  echo "npm install failed. Check the logs for more details."
+  exit 1
+fi
+
 npm start
 
+if [ $? -ne 0 ]; then
+  echo "npm start failed. Check the logs for more details."
+  exit 1
+fi
+
 sudo systemctl restart nginx
+
+if [ $? -ne 0 ]; then
+  echo "Nginx restart failed. Check the system logs for more details."
+  exit 1
+fi
+
+echo "Deployment successful!"
