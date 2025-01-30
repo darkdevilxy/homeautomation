79513d25f6c8813d49949b2b80261d3dad952c5d
darkdevilxy <53143941+darkdevilxy@users.noreply.github.com>
2025-01-31 10:25:07 +1100

    Fixed the bug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 const path = require('path');
 
-ap.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'public', 'index.html'));
 });
